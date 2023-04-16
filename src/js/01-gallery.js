9606576058a3d641bd800a0be76078952a20f0da
starik061 <starik061@gmail.com>
2023-04-16 19:52:48 +0300

    made Task #1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+createGallery(galleryItems);
+
+function createGallery(galleryData) {
+  gallery.innerHTML = galleryData
+    .map(
+      galleryItem => `<li class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+  </a>
+</li>`
+    )
+    .join('');
+}
+
+let galleryModalWindow = new SimpleLightbox('.gallery__link', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
